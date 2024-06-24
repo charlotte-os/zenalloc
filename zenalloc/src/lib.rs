2f12dbb1f68ccff2cc7be5d106e573f60e2ffa0d
parker <129785640+parkerstafford@users.noreply.github.com>
2024-06-24 16:00:34 -0700

    Make alloc_trait and update lib

--- zenalloc/src/lib.rs
+++ zenalloc/src/lib.rs
@@ -1,3 +1,8 @@
+#![no_std]
+
+pub mod alloc_trait;
+mod alloc_trait;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -5,10 +10,36 @@ pub fn add(left: usize, right: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::alloc_trait::{Alloc, SimpleAllocator};
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_alloc_zeroed() {
+        let allocator = SimpleAllocator;
+        let size = 1024;
+        let ptr = allocator.alloc_zeroed(size).expect("Allocation failed");
+        unsafe {
+            for i in 0..size {
+                assert_eq!(*ptr.as_ptr().add(i), 0);
+            }
+        }
+        allocator.dealloc(ptr, size);
+    }
+
+    #[test]
+    fn test_realloc() {
+        let allocator = SimpleAllocator;
+        let size = 1024;
+        let ptr = allocator.alloc(size).expect("Allocation failed");
+
+        let new_size = 2048;
+        let new_ptr = allocator.realloc(ptr, new_size).expect("Reallocation failed");
+
+        allocator.dealloc(new_ptr, new_size);
     }
 }
